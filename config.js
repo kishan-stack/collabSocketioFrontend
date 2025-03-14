1aa781bb36deeab691a7fef36940dcaa14fc7aa4
kishan <varmajaykishan63@gmail.com>
2025-03-14 17:19:43 +0530

    config update frontend update v7

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
     development: { Url: "http://localhost:5000/api/v1" },
-    production: { Url: "https://c511-103-171-118-138.ngrok-free.app/api/v1" },
+    production: { Url:process.env.NEXT_PUBLIC_BACKEND_API_URL },
 };
 export default config;
